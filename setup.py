ffae26abfa36101dc862a9772c87778f47cd1beb
dchilot <dchilot@hotmail.com>
2014-05-02 16:51:10 +0000

    Add missing dependency. Add tox configuration to test on various python versions easily. Add classifiers.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import setuptools
 
 # Hack to prevent stupid TypeError: 'NoneType' object is not callable error on
 # exit of python setup.py test # in multiprocessing/util.py _exit_function when
@@ -12,19 +12,28 @@ try:
 except ImportError:
     pass
 
-setup(
+setuptools.setup(
     name='orwell.agent',
     version='0.0.1',
     description='Agent connecting to the game server.',
     author='',
     author_email='',
-    packages=find_packages(exclude="test"),
+    packages=setuptools.find_packages(exclude="test"),
     test_suite='nose.collector',
-    install_requires=['pyzmq'],
+    install_requires=['pyzmq', 'cliff'],
     tests_require=['nose', 'coverage', 'mock'],
     entry_points={
         'console_scripts': [
             'thought_police = orwell.agent.main:main',
         ]
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.3'],
 )
